8020785b2c204490dad7167c1ebd5a57dc0bd917
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2025-03-29 18:54:19 +0700

    updated vị trí

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,10 +14,10 @@ const Page = () => {
         <div id='your-smoother-element-id' data-hero className='h-full flex flex-col bg-gray-100 '>
             <SesionHome className='px-8 ' />
             <SesionOtherText />
-            <SesionAbout className='px-8' />
             {/* <SesionDetailAbout className='px-8' /> */}
             <SesionSkills className='px-8' />
             <SesionProject className='px-8' />
+            <SesionAbout className='px-8' />
         </div>
     )
 }
