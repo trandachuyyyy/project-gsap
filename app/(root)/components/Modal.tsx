89da9abcb9762f547563f06afb6c971c17e1cff1
Trần Đắc Huy <79629158+trandachuy2002@users.noreply.github.com>
2025-05-17 14:43:50 +0700

    updated

--- app/(root)/components/Modal.tsx
+++ app/(root)/components/Modal.tsx
@@ -10,11 +10,18 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function ModalCanvas() {
     return (
-        <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-            <Suspense fallback={null}>
-                <SceneContent />
-            </Suspense>
-        </Canvas>
+        <div className="relative w-full h-[100vh] md:h-[80vh]">
+            <Canvas className="absolute inset-0 w-full h-full" camera={{ position: [0, 0, 5], fov: 50 }}>
+                <Suspense fallback={null}>
+                    <SceneContent />
+                </Suspense>
+            </Canvas>
+        </div>
+        // <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+        //     <Suspense fallback={null}>
+        //         <SceneContent />
+        //     </Suspense>
+        // </Canvas>
     );
 }
 function SceneContent() {
